845198abb5acf33b0916cb801ec65f69f0ff4995
Miguel Silva <miguelsilv98@gmail.com>
2022-02-10 15:26:29 -0300

    Adicionando navegação

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,75 @@ class MyApp extends StatelessWidget {
     return FluentApp(
       title: 'Flutter Demo',
       theme: ThemeData(),
+      home: NavigatorPage(),
+    );
+  }
+}
+
+class NavigatorPage extends StatefulWidget {
+  @override
+  State<NavigatorPage> createState() => _NavigatorPageState();
+}
+
+class _NavigatorPageState extends State<NavigatorPage> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationView(
+      pane: NavigationPane(
+        displayMode: PaneDisplayMode.open,
+        selected: index,
+        onChanged: (int currentIndex) {
+          setState(() {
+            index = currentIndex;
+          });
+        },
+        items: [
+          PaneItem(
+            icon: Icon(
+              FluentIcons.home,
+            ),
+            title: Text("Pagina 1"),
+          ),
+          PaneItem(
+            icon: Icon(
+              FluentIcons.accounts,
+            ),
+            title: Text("Pagina 2"),
+          ),
+          PaneItem(
+            icon: Icon(
+              FluentIcons.accounts,
+            ),
+            title: Text("Pagina 3"),
+          ),
+        ],
+      ),
+      content: NavigationBody(
+        index: index,
+        children: [
+          FirstPage(),
+          SecondPage(),
+          FirstPage(),
+        ],
+      ),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
     );
   }
 }
